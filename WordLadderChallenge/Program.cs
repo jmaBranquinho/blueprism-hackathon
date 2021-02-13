c61937d416a069ceeeb3e07bf532183ae2efaa9f
Jorge Branquinho <jorge.de_albuquerque_branquinho.ext@siemens.com>
2021-02-13 21:09:08 +0000

    Created both source and unit test projects

---
+++ WordLadderChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordLadderChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
